6e2affcbf270b73c6f52d1bac1900103bfcda762
jichani <jichan7571@naver.com>
2023-09-08 21:29:07 +0900

    ejs를 렌더하는 코드 추가

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+// http로 서버 띄워보기. http는 내장 모듈. 불편한 점이 많아서 express를 사용함.
+
+// express로 서버 띄워보기. npm i express로 설치함
 const express = require("express");
 const app = express();
 
+// 앱 세팅. 기본 경로를 설정하는 코드
+app.set("views", "./views");
+// 어떤 파일을 볼 것인지를 설정하는 코드
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("여기는 루트입니다.");
+  res.render("home/index");
 })
 
 app.get("/login", (req, res) => {
-  res.send("여기는 로그인 화면입니다.");
+  res.render("home/login");
 })
 
 app.listen(3000, function () {
