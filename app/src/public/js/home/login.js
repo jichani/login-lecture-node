3530899306ee20449457ec5ca2d8079703aefc71
jichani <jichan7571@naver.com>
2023-09-09 09:02:42 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,16 @@ function login() {
     id: id.value,
     psword: psword.value,
   };
+
+  // fetch로 프론트에서 서버로 데이터 보내기. 첫번째 파라미터로 어떠한 경로로 데이터를 보낼지 결정. 로그인이라는 경로로 보낸다고 가정. 두번째 파라미터로 어떠한 데이터를 보낼지 정할 수 있다. 바디를 통해서 데이터를 전달 가능. 이 때 메서드도 전달해야 한다.
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+
   console.log(req);
+  console.log(JSON.stringify(req));
 }
