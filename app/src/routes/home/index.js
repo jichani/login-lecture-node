95dbabe441eed2de2b8e5f4890af23aeff1f1072
jichani <jichan7571@naver.com>
2023-09-20 22:23:02 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,10 +7,10 @@ const router = express.Router();
 // 불러오기
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.output.home)
-router.get("/login", ctrl.output.login)
-
-router.post("/login", ctrl.process.login)
+router.get("/", ctrl.output.home);
+router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
+router.post("/login", ctrl.process.login);
 
 // 라우터를 사용할 수 있도록 외부로 던져준다.
 module.exports = router;
