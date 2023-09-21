22f82255f6a47fcb83de995a805245f5be33e2aa
jichani <jichan7571@naver.com>
2023-09-21 17:55:14 +0900

    fs(file system) 모듈을 통한 로그인 기능 구현

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -1,26 +1,18 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
   // # 표시로 private하게 만든다.
-  static #users = {
-    id: ["woorimIT", "나는준현이당", "김팀장"],
-    psword: ["1234", "1234", "123456"],
-    name: ["우리밋", "준현", "김팀장"],
-  };
+  // static #users = {
+  //   id: ["woorimIT", "나는준현이당", "김팀장"],
+  //   psword: ["1234", "1234", "123456"],
+  //   name: ["우리밋", "준현", "김팀장"],
+  // };
 
-  static getUsers(...fields) {
-    const users = this.#users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
-
-  static getUserInfo(id) {
-    const users = this.#users;
+  // 프라이빗한 메서드는 항상 최상단에 올려주어야한다.
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const usersKeys = Object.keys(users); // => [id, psword, name]으로 만들어진다.
     const userInfo = usersKeys.reduce((newUser, info) => {
@@ -31,8 +23,30 @@ class UserStorage {
     return userInfo;
   }
 
+  static getUsers(...fields) {
+    // const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
+
+  static getUserInfo(id) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
+  }
+
+
+
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
