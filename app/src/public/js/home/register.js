d1b646e696a2ac303419b72ddf90010ad421ba8e
jichani <jichan7571@naver.com>
2023-09-21 17:10:58 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,15 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (!name.value) return alert("이름을 입력해주십시오.");
+  if (psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
+
+
   const req = {
     id: id.value,
     name: name.value,
     psword: psword.value,
-    confirmPsword: confirmPsword.value,
   };
 
   // fetch로 프론트에서 서버로 데이터 보내기. 첫번째 파라미터로 어떠한 경로로 데이터를 보낼지 결정. 로그인이라는 경로로 보낸다고 가정. 두번째 파라미터로 어떠한 데이터를 보낼지 정할 수 있다. 바디를 통해서 데이터를 전달 가능. 이 때 메서드도 전달해야 한다.
@@ -36,5 +40,5 @@ function register() {
     .catch((err) => {
       // 빨간색 에러 메시지 띄우게 구현
       console.error(new Error("회원가입 중 에러 발생"));
-    })
+    });
 };
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -10,7 +10,9 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
+
 router.post("/login", ctrl.process.login);
+router.post("/register", ctrl.process.register);
 
 // 라우터를 사용할 수 있도록 외부로 던져준다.
 module.exports = router;
