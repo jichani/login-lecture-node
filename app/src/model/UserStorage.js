ecd892de939f06226f558c291f487635c24ff071
jichani <jichan7571@naver.com>
2023-09-21 21:46:38 +0900

    fs 모듈을 통한 회원가입 기능 구현. 파일 db로 회원가입 구현 /

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -23,8 +23,9 @@ class UserStorage {
     return userInfo;
   }
 
-  static getUsers(...fields) {
-    // const users = this.#users;
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) return users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -32,6 +33,15 @@ class UserStorage {
       return newUsers;
     }, {});
     return newUsers;
+  }
+
+  static getUsers(isAll, ...fields) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUsers(data, isAll, fields);
+      })
+      .catch(console.error);
   }
 
   static getUserInfo(id) {
@@ -43,14 +53,18 @@ class UserStorage {
       .catch(console.error);
   }
 
-
-
-  static save(userInfo) {
-    // const users = this.#users;
+  static async save(userInfo) {
+    // "id", "psword", "name" 모든 데이터를 가져오고 싶을 때 true를 적으면 된다.
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      throw "이미 존재하는 아이디입니다.";
+    }
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
-    return { success: true };
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+    return { success: true }
+    // 데이터 추가
   }
 };
 
