65bc44056580417edfd9c05b25a40539c91775f5
jichani <jichan7571@naver.com>
2023-09-23 21:50:01 +0900

    로그 관리 모듈 winston 등록 및 설정 구현

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-"use strict";
 //이크마 스크립트 문법을 준수하겠다는 표현 사용!
+"use strict";
 
 // http로 서버 띄워보기. http는 내장 모듈. 불편한 점이 많아서 express를 사용함.
 
--- app/bin/www.js
+++ app/bin/www.js
@@ -3,9 +3,10 @@
 // bin 폴더는 일반적으로 애플리케이션의 시작점을 정의하는 스크립트를 포함.
 
 const app = require("../app");
+const logger = require("../src/config/logger");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("서버 가동");
+  logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
 });
