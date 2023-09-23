383e3ceefba9e19be5b22aff46fe91538373802f
jichani <jichan7571@naver.com>
2023-09-23 23:40:15 +0900

    병합 오류 수정 | 백엔드 맛보기 최종 최적화

--- app/src/config/log.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const appRoot = require("app-root-path");
-
-// "append" 모드로 작동하는 쓰기 스트림을 생성.
-var accessLogStream = fs.createWriteStream(
-  `${appRoot}/log/access.log`,
-  { flags: 'a' })
-
-module.exports = accessLogStream;
