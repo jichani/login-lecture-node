7d87bc3a9ddd9550db5adff174ebea4452927a95
jichani <jichan7571@naver.com>
2023-09-23 16:11:43 +0900

    AWS RDS로 회원가입 구현 | 웹서버와 WAS | 우리는 WAS 구현

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -3,45 +3,12 @@
 const db = require("../config/db");
 
 class UserStorage {
-  // # 표시로 private하게 만든다.
-  // static #users = {
-  //   id: ["woorimIT", "나는준현이당", "김팀장"],
-  //   psword: ["1234", "1234", "123456"],
-  //   name: ["우리밋", "준현", "김팀장"],
-  // };
-
-  // 프라이빗한 메서드는 항상 최상단에 올려주어야한다.
-  static #getUserInfo(data, id) {
-    const users = JSON.parse(data);
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // => [id, psword, name]으로 만들어진다.
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-
-    return userInfo;
-  }
-
-  static #getUsers(data, isAll, fields) {
-    const users = JSON.parse(data);
-    if (isAll) return users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
-
-  static getUsers(isAll, ...fields) {
-  }
 
   static getUserInfo(id) {
     return new Promise((resolve, reject) => {
-      db.query("SELECT * FROM users WHERE id = ?", [id], (err, data) => {
-        if (err) reject(err);
+      const query = "SELECT * FROM users WHERE id = ?;";
+      db.query(query, [id], (err, data) => {
+        if (err) reject(`${err}`);
         resolve(data[0]);
       })
     })
@@ -49,6 +16,14 @@ class UserStorage {
   // 4:!5
 
   static async save(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
+      db.query(query, [userInfo.id, userInfo.name, userInfo.psword],
+        (err) => {
+          if (err) reject(`${err}`);
+          resolve({ success: true });
+        });
+    });
   }
 };
 
